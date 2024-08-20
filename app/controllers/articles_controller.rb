b7e41ad0d37a72bcafa6e934cbace13154a9774c
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-08-20 16:14:48 -0400

    Added identical article title handling.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    data = HTTP.get("https://newsapi.org/v2/everything?q=bitcoin&apiKey=#{ENV["N_KEY"]}").parse
-    render json: data["articles"]
+    data = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search_terms]}&apiKey=#{ENV["N_KEY"]}").parse
+    render json: data["articles"].uniq{ |article| article["title"] }
   end
 end
